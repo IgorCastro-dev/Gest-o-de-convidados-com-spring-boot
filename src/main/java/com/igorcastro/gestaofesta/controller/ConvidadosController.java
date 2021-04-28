9fc2761467d73a94d8361f0487e14fb082fb9cbf
Igor Castro <igorjunqueira19@gmail.com>
2021-04-28 01:44:53 -0300

    criando a pagina que lista todos os convidados

--- src/main/java/com/igorcastro/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/igorcastro/gestaofesta/controller/ConvidadosController.java
@@ -1,13 +1,24 @@
 package com.igorcastro.gestaofesta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.igorcastro.gestaofesta.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
 	
+	@Autowired
+	private Convidados convidados;
+	
 	@RequestMapping("/convidados")
-	public String listar() {
-		return "convidados";
-	}
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("convidadosview");
+		modelAndView.addObject("convidados", convidados.findAll());
+		return modelAndView;
+		}
+
+	
 }
--- src/main/java/com/igorcastro/gestaofesta/repository/Convidados.java
+++ src/main/java/com/igorcastro/gestaofesta/repository/Convidados.java
@@ -1,5 +1,8 @@
 package com.igorcastro.gestaofesta.repository;
 
-public interface Convidados {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.igorcastro.gestaofesta.model.Convidado;
+
+public interface Convidados extends JpaRepository<Convidado, Long> {
 }
