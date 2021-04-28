a3a460d4a2abf256cab8e2944c8b3be9ae54d82c
Igor Castro <igorjunqueira19@gmail.com>
2021-04-28 14:48:17 -0300

    terminando o formulário e a lista de convidados

--- src/main/java/com/igorcastro/gestaofesta/controller/ConvidadosController.java
+++ src/main/java/com/igorcastro/gestaofesta/controller/ConvidadosController.java
@@ -1,11 +1,16 @@
 package com.igorcastro.gestaofesta.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.igorcastro.gestaofesta.model.Convidado;
 import com.igorcastro.gestaofesta.repository.Convidados;
+
+
 
 @Controller
 public class ConvidadosController {
@@ -13,12 +18,17 @@ public class ConvidadosController {
 	@Autowired
 	private Convidados convidados;
 	
-	@RequestMapping("/convidados")
-	public ModelAndView listar() {
+	@RequestMapping(value="/convidados", method=RequestMethod.GET)
+	public ModelAndView listar(Convidado mensagem) {
 		ModelAndView modelAndView = new ModelAndView("convidadosview");
 		modelAndView.addObject("convidados", convidados.findAll());
 		return modelAndView;
 		}
 
+	@RequestMapping(value="/convidados", method=RequestMethod.POST)
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
+	}
 	
 }
